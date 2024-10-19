c58e84cbdd3b446b6d73166541bab228a9fc9f46
ParkGiraffe <op5321@soongsil.ac.kr>
2024-10-19 14:21:32 +0900

    Feat: 구글 서치 콘솔 등록 작업

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,9 @@ export const metadata = {
   title: "원신 자기소개 표 생성기",
   description:
     "원신 유저들의 트친소, x친소 자기소개 표로 사용되는 이미지를 자동으로 생성해주는 사이트입니다.",
+  verification: {
+    google: "wGojUcGGwbwxOatwNRWiei1Bg6Xtq_f68Tc9XPAHLe0",
+  }
 };
 
 export default function RootLayout({ children }) {
